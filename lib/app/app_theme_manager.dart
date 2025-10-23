b9c9b03301d6c8cae598c0295a4ec2af7084f880
rbtc2 <rbtc22@gmail.com>
2025-10-23 11:00:53 +0900

    notosans

--- lib/app/app_theme_manager.dart
+++ lib/app/app_theme_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../constants/app_constants.dart';
 
 /// 앱 테마 관리 클래스
@@ -11,9 +12,16 @@ class AppThemeManager {
         brightness: Brightness.light,
       ),
       useMaterial3: true,
-      appBarTheme: const AppBarTheme(
+      // Noto Sans KR 폰트 적용 (한국어 최적화)
+      textTheme: GoogleFonts.notoSansTextTheme(),
+      appBarTheme: AppBarTheme(
         elevation: 0,
         centerTitle: true,
+        titleTextStyle: GoogleFonts.notoSans(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: const Color(AppColors.primaryColorValue),
+        ),
       ),
       cardTheme: CardThemeData(
         elevation: 2,
@@ -34,9 +42,16 @@ class AppThemeManager {
         brightness: Brightness.dark,
       ),
       useMaterial3: true,
-      appBarTheme: const AppBarTheme(
+      // Noto Sans KR 폰트 적용 (한국어 최적화)
+      textTheme: GoogleFonts.notoSansTextTheme(ThemeData.dark().textTheme),
+      appBarTheme: AppBarTheme(
         elevation: 0,
         centerTitle: true,
+        titleTextStyle: GoogleFonts.notoSans(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
       ),
       cardTheme: CardThemeData(
         elevation: 2,
